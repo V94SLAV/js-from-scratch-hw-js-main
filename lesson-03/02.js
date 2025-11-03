c97b90f327b62e60532a4bbfde7f2236b90c4220
Viacheslav Korolev <vyach.korolev@gmail.com>
2025-11-03 11:11:16 +0300

    L03T02 complete

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,13 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+function isEven(number) {
+    let verification
+    let even = number%2
+    if (even === 0){
+        verification = true
+    } else {
+        verification = false
+    }
+    return verification
+}
