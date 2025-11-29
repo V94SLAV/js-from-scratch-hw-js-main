3f321b7278f6507b8e9f6206bc14bd618b520629
Viacheslav Korolev <vyach.korolev@gmail.com>
2025-11-29 11:06:01 +0300

    New solution

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -44,6 +44,3 @@ function updateGallery(object, name, info) {
   }
   }
 }
-updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506')
-updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali')
-console.log(gallery);
